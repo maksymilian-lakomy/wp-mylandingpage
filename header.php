8437d2a99ec5da086387c5cccd2b8755617be3f7
Maksymilian Łakomy <zommer128@gmail.com>
2020-11-17 22:24:30 +0100

    Clear old styling

--- header.php
+++ header.php
@@ -3,17 +3,5 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<title><? bloginfo("name") . wp_title(); ?></title>
 
-	<script src="https://use.fontawesome.com/c59d990f16.js"></script>
-
-	<link
-		href="https://fonts.googleapis.com/css?family=Lato:400,700&display=swap"
-		rel="stylesheet"
-	/>
-	<link
-		rel="stylesheet"
-		type="text/css"
-		href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"
-	/>
-
 	<?php wp_head(); ?>
 </head>
