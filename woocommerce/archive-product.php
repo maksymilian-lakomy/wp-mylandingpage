32c3430bf4dfce7c824052a7a5a1d0ceb588dc23
Michał Starski <michalstarski99@gmail.com>
2020-11-17 23:39:19 +0100

    Init static files on the front page

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -1,2 +1,16 @@
-<?php
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<?php get_header(); ?>
+
+<body>
+
+  <?php
+    load_template(get_template_directory() . '/templates/aside.php');
+  ?>
+
+	<?php get_footer(); ?>
+</body>
+
+</html>
 
