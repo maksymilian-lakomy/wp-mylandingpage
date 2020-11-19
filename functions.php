f2a51281acb140f170c614bedbf52d95c2e3b426
Michał Starski <michalstarski99@gmail.com>
2020-11-19 22:16:19 +0100

    Added content controls for index aside element

--- functions.php
+++ functions.php
@@ -22,3 +22,37 @@ function mytheme_add_woocommerce_support()
 
 add_action('wp_enqueue_scripts', 'add_external_files');
 add_action('after_setup_theme', 'mytheme_add_woocommerce_support');
+
+function shop_customize_register($wp_customize)
+{
+	$wp_customize->add_section('homepage', array(
+		'title' => __('Strona główna', 'shop'),
+		'description' => 'Strona główna sklepu',
+		'priority' => 1
+	));
+
+	$wp_customize->add_setting('aside_title', array(
+		'default' => 'Donec volutpat dui tellus, at consequat turpis consequat nec.',
+		'type' => 'theme_mod'
+	));
+
+	$wp_customize->add_control('aside_title', array(
+		'label' => __('Tytuł przerywnika', 'shop'),
+		'section' => 'homepage',
+		'priority' => 1
+	));
+
+	$wp_customize->add_setting('aside_subtitle', array(
+		'default' => 'Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym.',
+		'type' => 'theme_mod'
+	));
+
+	$wp_customize->add_control('aside_subtitle', array(
+		'label' => __('Opis przerywnika', 'shop'),
+		'section' => 'homepage',
+		'priority' => 1
+	));
+}
+
+
+add_action('customize_register', 'shop_customize_register');
--- templates/index-description.php
+++ templates/index-description.php
@@ -1,9 +1,9 @@
 <aside class="index-description">
   <img class="index-description__sewing-machine" src="<?php echo ASSETS_PATH ?>sewing-machine.svg" alt="sewing-machine">
   <p class="index-description__header">
-    Donec volutpat dui tellus, at consequat turpis consequat nec.
+    <?php echo get_theme_mod('aside_title') ?>
   </p>
   <p class="index-description__content">
-    Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym.
+    <?php echo get_theme_mod('aside_subtitle') ?>
   </p>
 </aside>
