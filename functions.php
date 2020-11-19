1ff2ee54f72202374994422cbf7581743b3b7c17
Michał Starski <michalstarski99@gmail.com>
2020-11-19 21:49:09 +0100

    Created index page aside element - mobile ver.

--- functions.php
+++ functions.php
@@ -1,11 +1,12 @@
 <?php
 
 // Constants
-define('ASSETS_PATH', get_template_directory() . '/assets/');
+define('ASSETS_PATH', get_template_directory_uri() . '/assets/');
 define('TEMPLATES_PATH', get_template_directory() . '/templates/');
 
-function __template(string $name) {
-  return load_template(TEMPLATES_PATH . $name . '.php');
+function __template(string $name)
+{
+	return load_template(TEMPLATES_PATH . $name . '.php');
 }
 
 function add_external_files()
@@ -14,9 +15,10 @@ function add_external_files()
 	wp_enqueue_script('main', get_template_directory_uri() . '/js/main.js', array('jquery'), false, true);
 }
 
-function mytheme_add_woocommerce_support() {
-	add_theme_support( 'woocommerce' );
+function mytheme_add_woocommerce_support()
+{
+	add_theme_support('woocommerce');
 }
 
 add_action('wp_enqueue_scripts', 'add_external_files');
-add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
+add_action('after_setup_theme', 'mytheme_add_woocommerce_support');
